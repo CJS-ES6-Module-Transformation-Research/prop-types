eb2b0ec8a0f0fca8bc32051bf09485ac2a639e4c
Ellen Arteca <ellen.arteca@gmail.com>
2020-08-22 15:27:51 -0400

    remove conditional export

--- index.js
+++ index.js
@@ -5,15 +5,18 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+var toExport = undefined;
 if (process.env.NODE_ENV !== 'production') {
   var ReactIs = require('react-is');
 
   // By explicitly using `prop-types` you are opting into new development behavior.
   // http://fb.me/prop-types-in-prod
   var throwOnDirectAccess = true;
-  module.exports = require('./factoryWithTypeCheckers')(ReactIs.isElement, throwOnDirectAccess);
+  toExport = require('./factoryWithTypeCheckers')(ReactIs.isElement, throwOnDirectAccess);
 } else {
   // By explicitly using `prop-types` you are opting into new production behavior.
   // http://fb.me/prop-types-in-prod
-  module.exports = require('./factoryWithThrowingShims')();
+  toExport = require('./factoryWithThrowingShims')();
 }
+
+module.exports = toExport;
